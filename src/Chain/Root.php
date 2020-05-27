3965cdf89c5933ed242f02c58c2f1fde1a97ca1f
goosman.lei <goosman.lei@gmail.com>
2020-05-28 02:04:20 +0800

    fixbug

--- src/Chain/Root.php
+++ src/Chain/Root.php
@@ -3,6 +3,6 @@ namespace Microbe\Chain;
 class Root extends \Microbe\Chain {
     public function exec($request, $response) {
         $this->doNext($request, $response);
-        $response->output();
+        $response->success();
     }
 }
