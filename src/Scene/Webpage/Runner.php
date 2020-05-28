539d404201f7a8665694aed9f087d83b8dcdfd69
goosman.lei <goosman.lei@gmail.com>
2020-05-28 15:30:16 +0800

    fixbug

--- src/Scene/Webpage/Runner.php
+++ src/Scene/Webpage/Runner.php
@@ -16,9 +16,9 @@ class Runner {
         \Microbe\Microbe::init($config);
 
         \Microbe\Microbe::$ins->appendChain(new \Microbe\Chain\Root(), '-root');
-        \Microbe\Microbe::$ins->appendChain(new \Microbe\Cgi\Chain\CanonicalUri($config->get('scene.webpage.canonicalUri'), '-canonical-uri');
-        \Microbe\Microbe::$ins->appendChain(new \Microbe\Cgi\Chain\Router($config->get('scene.webpage.router'), '-route');
-        \Microbe\Microbe::$ins->appendChain(new \Microbe\Cgi\Chain\Dispatcher($config->get('scene.webpage.dispatcher'), '-dispatch');
+        \Microbe\Microbe::$ins->appendChain(new \Microbe\Cgi\Chain\CanonicalUri($config->get('scene.webpage.canonicalUri')), '-canonical-uri');
+        \Microbe\Microbe::$ins->appendChain(new \Microbe\Cgi\Chain\Router($config->get('scene.webpage.router')), '-route');
+        \Microbe\Microbe::$ins->appendChain(new \Microbe\Cgi\Chain\Dispatcher($config->get('scene.webpage.dispatcher')), '-dispatch');
         \Microbe\Microbe::$ins->installUserChain();
     }
 
