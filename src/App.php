22733cc60d90111690b34246a6c74a95e508d17f
goosman.lei <goosman.lei@gmail.com>
2020-06-16 01:43:16 +0800

    fixbug

--- src/App.php
+++ src/App.php
@@ -8,6 +8,6 @@ class App {
     public function __construct($config) {
         $this->config = $config;
         $this->serviceFactory  = new \Microbe\Service\Factory($config->get('app.service'));
-        $this->resourceFactory = new \Microbe\Service\Factory($config->get('app.resource'));
+        $this->resourceFactory = new \Microbe\Resource\Factory($config->get('app.resource'));
     }
 }
