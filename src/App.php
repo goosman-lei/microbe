1dd1df07770e293c57a75aef6ac63c446165a565
goosman.lei <goosman.lei@gmail.com>
2020-06-15 20:17:58 +0800

    add to app

--- src/App.php
+++ src/App.php
@@ -3,9 +3,11 @@ namespace Microbe;
 class App {
     public $config;
     public $serviceFactory;
+    public $resourceFactory;
 
     public function __construct($config) {
         $this->config = $config;
         $this->serviceFactory = new \Microbe\Service\Factory($config->get('app.service'));
+        $this->resourceFactory = new \Microbe\Resource\Factory($config->get('app.resource'));
     }
 }
